f6532cd58b6641550af1716b2f6245cc023d0605
levant12 <leo.tsero@gmail.com>
2023-07-02 19:29:20 +0400

    change image file path handling

--- src/main/java/ge/restaurant/impl/ImageImpl.java
+++ src/main/java/ge/restaurant/impl/ImageImpl.java
@@ -13,11 +13,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.util.ResourceUtils;
+
 
 @Service
 public class ImageImpl {
@@ -29,51 +36,68 @@ public class ImageImpl {
     private MenuRepository menuRepository;
 
 
-    private final String FOLDER_PATH = "C:\\Users\\DELL\\Desktop\\MyFile";
-    public String uploadImageToFileSystem(List<MultipartFile> files,Long id) throws IOException {
+    public String uploadImageToFileSystem(List<MultipartFile> files, Long id) throws IOException {
         Restaurant restaurant = restaurant(id);
+        ClassLoader classLoader = getClass().getClassLoader();
+        File resourcesDirectory = new File(classLoader.getResource("assets/images").getPath());
+        String folderPath = URLDecoder.decode(String.valueOf(resourcesDirectory), StandardCharsets.UTF_8);
+
         for (MultipartFile file : files) {
-            UUID uuid=UUID.randomUUID();
-            String name=uuid+"_"+file.getOriginalFilename();
+            UUID uuid = UUID.randomUUID();
+            String name = uuid + "_" + file.getOriginalFilename();
 
-            String filePath = FOLDER_PATH +"/"+ name;
+            String filePath = folderPath + File.separator + name;
 
-            ImageData imageData = new ImageData(name,
-                            file.getContentType(), filePath);
+            ImageData imageData = new ImageData(name, file.getContentType(), filePath);
             restaurant.getImages().add(imageData);
 
             file.transferTo(new File(filePath));
             restaurantRepository.save(restaurant);
         }
-        return "image uploaded successfully";
+        return "Image uploaded successfully";
     }
 
     public byte[] downloadImage(String name) throws IOException {
-        Optional<ImageData> imageData = imageRepository.findByName(name);
-        String filePath=imageData.get().getFilePath();
-        return Files.readAllBytes(new File(filePath).toPath());
+        ClassLoader classLoader = getClass().getClassLoader();
+        URI uri = null;
+        try {
+            uri = classLoader.getResource("assets/images").toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        String folderPath = Paths.get(uri).toString();
+        String imageFilePath = folderPath + File.separator + name;
+
+        Path imagePath = Paths.get(imageFilePath);
+        return Files.readAllBytes(imagePath);
     }
 
 
-    public String uploadImage(List<String> id,List<String> foodName,List<MultipartFile> file) throws IOException {
+    public String uploadImage(List<String> id, List<String> foodName, List<MultipartFile> files) throws IOException {
         for (int i = 0; i < id.size(); i++) {
-            Long l=Long.parseLong(id.get(i));
-            String name=foodName.get(i);
-            MultipartFile image=file.get(i);
+            Long l = Long.parseLong(id.get(i));
+            String name = foodName.get(i);
+            MultipartFile file = files.get(i);
+
             Menu_Items menuItems = menuRepository.findByNameAndID(name, l);
-            UUID uuid=UUID.randomUUID();
-            String imageName=uuid+"_"+image.getOriginalFilename().replaceAll("\\s","");
-            String filePath = FOLDER_PATH +"/"+ imageName;
-            ImageData imageData = new ImageData(imageName,
-                    image.getContentType(), filePath);
+
+            UUID uuid = UUID.randomUUID();
+            String imageName = uuid + "_" + file.getOriginalFilename().replaceAll("\\s", "");
+
+            String folderPath = ResourceUtils.getFile("classpath:assets/images").getAbsolutePath();
+            String filePath = folderPath + File.separator + imageName;
+
+            ImageData imageData = new ImageData(imageName, file.getContentType(), filePath);
             menuItems.setImageData(imageData);
             menuRepository.save(menuItems);
-            image.transferTo(new File(filePath));
+
+            file.transferTo(new File(filePath));
         }
+
         return "Image upload successfully";
     }
 
-    private Restaurant restaurant(Long id){
+    private Restaurant restaurant(Long id) {
         return restaurantRepository.findById(id).get();
     }
 }
